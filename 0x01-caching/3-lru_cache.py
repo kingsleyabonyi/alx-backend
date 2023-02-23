74fdcb5e01087a7bb1ef5fd09ff9b50278303608
kingsleyabonyi <kinabonyi@gmail.com>
2023-02-23 02:31:38 +0100

    LRU caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,21 +1,19 @@
-
 #!/usr/bin/env python3
 """ BaseCaching module
 """
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """
     FIFOCache defines a FIFO caching system
     """
-
     def __init__(self):
         """
         Initialize the class with the parent's init method
         """
         super().__init__()
-        self.order = []
+        self.usage = []
 
     def put(self, key, item):
         """
@@ -26,12 +24,12 @@ class LIFOCache(BaseCaching):
         else:
             length = len(self.cache_data)
             if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.order[-1]))
-                del self.cache_data[self.order[-1]]
-                del self.order[-1]
-            if key in self.order:
-                del self.order[self.order.index(key)]
-            self.order.append(key)
+                print("DISCARD: {}".format(self.usage[0]))
+                del self.cache_data[self.usage[0]]
+                del self.usage[0]
+            if key in self.usage:
+                del self.usage[self.usage.index(key)]
+            self.usage.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
@@ -39,5 +37,7 @@ class LIFOCache(BaseCaching):
         Return the value linked to a given key, or None
         """
         if key is not None and key in self.cache_data.keys():
+            del self.usage[self.usage.index(key)]
+            self.usage.append(key)
             return self.cache_data[key]
         return None
