00b859c13fd45fe908d5437d0ed72abf3d770968
kingsleyabonyi <kinabonyi@gmail.com>
2023-02-23 02:35:35 +0100

    100 LFU caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -4,7 +4,7 @@
 from base_caching import BaseCaching
 
 
-class MRUCache(BaseCaching):
+class LFUCache(BaseCaching):
     """
     FIFOCache defines a FIFO caching system
     """
@@ -15,6 +15,7 @@ class MRUCache(BaseCaching):
         """
         super().__init__()
         self.usage = []
+        self.frequency = {}
 
     def put(self, key, item):
         """
@@ -25,9 +26,29 @@ class MRUCache(BaseCaching):
         else:
             length = len(self.cache_data)
             if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.usage[-1]))
-                del self.cache_data[self.usage[-1]]
-                del self.usage[-1]
+                lfu = min(self.frequency.values())
+                lfu_keys = []
+                for k, v in self.frequency.items():
+                    if v == lfu:
+                        lfu_keys.append(k)
+                if len(lfu_keys) > 1:
+                    lru_lfu = {}
+                    for k in lfu_keys:
+                        lru_lfu[k] = self.usage.index(k)
+                    discard = min(lru_lfu.values())
+                    discard = self.usage[discard]
+                else:
+                    discard = lfu_keys[0]
+
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
+                del self.usage[self.usage.index(discard)]
+                del self.frequency[discard]
+            # update usage frequency
+            if key in self.frequency:
+                self.frequency[key] += 1
+            else:
+                self.frequency[key] = 1
             if key in self.usage:
                 del self.usage[self.usage.index(key)]
             self.usage.append(key)
@@ -40,5 +61,6 @@ class MRUCache(BaseCaching):
         if key is not None and key in self.cache_data.keys():
             del self.usage[self.usage.index(key)]
             self.usage.append(key)
+            self.frequency[key] += 1
             return self.cache_data[key]
         return None
